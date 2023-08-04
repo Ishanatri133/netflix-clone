9e78b80a3b80044dab8b11f9130d650b39e7b065
Ishan Sharma <106100674+Ishanatri133@users.noreply.github.com>
2023-08-04 21:30:03 +0530

    Create Downloads.dart

---
+++ Downloads.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DOWNLOADS extends StatelessWidget {
+  const DOWNLOADS ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

babf15e8976e94a17cd9e8876d0ff85236364e1a
Ishan Sharma <106100674+Ishanatri133@users.noreply.github.com>
2023-08-04 21:29:37 +0530

    Create ComingSoon.dart

---
+++ ComingSoon.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ComingSoon extends StatelessWidget {
+  const ComingSoon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        "COMING SOON PAGE",
+        style: TextStyle( fontSize: 40, fontWeight: FontWeight.bold ),),
+    );
+
+  }
+}

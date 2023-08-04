87e7a9401c2b97638a4756888ea670ed8d52b5f4
Ishan Sharma <106100674+Ishanatri133@users.noreply.github.com>
2023-08-04 21:27:38 +0530

    Create main3.dart

---
+++ main3.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter_application_1/shared/widgets/navbar.dart';
+// import 'package:flutter_application_1/screen/secondpage.dart';
+// import 'package:flutter_application_1/screen/secondpage.dart';
+
+// import 'package:flutter_application_1/homepage.dart';
+import 'package:flutter_application_1/shared/widgets/navigation_bar.dart';
+void main(){
+  runApp(const MaterialApp(
+    home:NavBar()
+    
+     
+
+
+));
+}
+
